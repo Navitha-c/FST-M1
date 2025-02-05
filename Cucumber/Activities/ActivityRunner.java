1286ab127205f5d3224e50baefae8ffa1dbc7bb2
Navitha-c <Navitha.C@IBM.com>
2025-02-05 22:55:45 +0530

    Add files via upload

---
+++ Cucumber/Activities/ActivityRunner.java
@@ -0,0 +1,27 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+import org.junit.runner.RunWith; 
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		features = "Features",
+		glue = {"stepDefinitions"},
+		//run scenario which has either the tags
+		//tags = "@PromptAlert or @ConfirmAlert",
+		//run scenario which has both the tags
+		//tags = "@SmokeTest and @SimpleAlert",
+		tags = "@activity1 or @activity2 or @activity3 or @activity4 or @activity5",
+		plugin = {"pretty"},
+		//create HTML report file
+		//plugin = {"html: test-reports"},
+		//create JSON report file (we cant create folder we need to specifiy file name only
+		//plugin = {"json: json-report.json"},
+		monochrome = true
+			
+)
+
+public class ActivityRunner {}
