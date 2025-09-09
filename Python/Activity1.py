2a18b04ff1e4548e78f3635d6472433327ed74ce
Navitha-c <Navitha.C@IBM.com>
2025-09-09 11:00:17 +0530

    Add files via upload

---
+++ Python/Activity1.py
@@ -0,0 +1,10 @@
+name= input("enter your name")
+#take user's age as input
+age = int(input("Enter yor age:"))
+print(name)
+print(age)
+
+#calcualte the year they turn 100
+year_100 = (100-age)+2025
+
+print(f"{name} will turn 100 in {year_100}")
---
+++ Python/Activity8.py
@@ -0,0 +1,10 @@
+num_list = [90,21,75,28,10,90]
+
+first_value = num_list[0]
+last_value = num_list[-1]
+
+print("first and last value are equal")
+if first_value == last_value:
+    print("true")
+else:
+    print("false")
